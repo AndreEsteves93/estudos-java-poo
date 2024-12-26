dc2c678b6db17af532f1eee85a8e3ea729c961e1
AndreEsteves93 <andre.esteves937@gmail.com>
2024-12-26 17:53:39 -0300

    Aula10

---
+++ Aula10/src/main/java/com/mycompany/aula10/Professor.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aula10;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Professor extends Pessoa {
+    
+    // Atributos
+    private String especialidade;
+    private float salario;
+
+    // Métodos únicos
+    public void receberAum(float aum) {
+        salario += aum;
+    }
+
+    // Métodos especiais
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+}
