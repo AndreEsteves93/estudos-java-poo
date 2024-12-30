06275b9532e6beec4586e6fa07de966edee43e1b
AndreEsteves93 <andre.esteves937@gmail.com>
2024-12-30 12:40:44 -0300

    Aula13

---
+++ Aula13/src/main/java/com/mycompany/aula13/Animal.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aula13;
+
+/**
+ * Classe abstrata
+ * root / raiz
+ */
+public abstract class Animal {
+
+    // Atributos
+    protected float peso;
+    protected int idade;
+    protected int membros;
+
+    // Métodos abstratos
+    public abstract void emitirSom();
+
+    // Métodos especiais
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getMembros() {
+        return membros;
+    }
+
+    public void setMembros(int membros) {
+        this.membros = membros;
+    }
+
+}
---
+++ Aula13/src/main/java/com/mycompany/aula13/Lobo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aula13;
+
+/**
+ * Subclasse de Mamifero
+ * descendente do root Animal
+ */
+public class Lobo extends Mamifero {
+
+    // Métodos de sobreposição
+    @Override
+    public void emitirSom() {
+        System.out.println("Auuuuuuuuuuuuu!");
+    }
+    
+}
